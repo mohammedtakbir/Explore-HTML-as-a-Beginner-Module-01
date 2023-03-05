467e8722d6e4ec6650f570e22627935a54ad5087
mohammedtakbir <takbirhossain678@gmail.com>
2023-03-05 18:22:14 +0600

    explore all input types

---
+++ js/input.js
@@ -0,0 +1,13 @@
+// document.querySelector('.radioInput').addEventListener('click', (e) => {
+//     console.log(e.target.innerText)
+// })
+
+//* input range
+// document.getElementById('range').addEventListener('change', (e) => {
+//     console.log(e.target.value)
+// })
+
+//* input search
+// document.getElementById('search-field').addEventListener('keyup', (e) => {
+//     console.log(e.target.value)
+// })
